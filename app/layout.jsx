f91f2dcf3b7a8e1bc8426e9ffef6b23fec8e7ffb
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-08-26 13:41:49 +0600

    navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import "@/assets/styles/globals.css";
+import Navbar from "@/components/Navbar";
 export const metadata = {
   title:"Property Plus",
   keywords:"rental, property, real estate",
@@ -8,6 +9,7 @@ const MainLayout = ({ children }) => {
   return (
     <html>
       <body>
+        <Navbar />
         <main>{children}</main>
       </body>
     </html>
