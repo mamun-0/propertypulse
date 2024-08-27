1aff9d0f2712bff82c038c94dde9971796892c1d
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-08-27 17:39:29 +0600

    Property model

---
+++ models/Property.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+const PropertySchema = new mongoose.Schema(
+  {
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    location: {
+      street: String,
+      city: String,
+      state: String,
+      zipcode: String,
+    },
+    beds: {
+      type: Number,
+      required: true,
+    },
+    baths: {
+      type: Number,
+      required: true,
+    },
+    square_feet: {
+      type: Number,
+      required: true,
+    },
+    amenities: [String],
+    rates: {
+      nightly: Number,
+      weekly: Number,
+      monthly: Number,
+    },
+    seller_info: {
+      name: String,
+      phone: String,
+      email: String,
+    },
+    images: [String],
+    is_featured: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Property", PropertySchema);
