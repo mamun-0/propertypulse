1d1b2fdaaf3049180228bd36b77b76fafcbeee95
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-08-27 11:26:47 +0600

    include metadata and Footer

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,19 @@
 import "@/assets/styles/globals.css";
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 export const metadata = {
-  title:"Property Plus",
-  keywords:"rental, property, real estate",
-  description:"Property Plus is a rental property management platform that helps landlords manage their properties and tenants easily.",
-}
+  title: "Property Pulse",
+  keywords: "rental, property, real estate",
+  description:
+    "Property Pulse is a rental property management platform that helps landlords manage their properties and tenants easily.",
+};
 const MainLayout = ({ children }) => {
   return (
     <html>
       <body>
         <Navbar />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
