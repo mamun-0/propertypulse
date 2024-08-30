fba05ac8c20009796cd3632c114995b6c9ef0e02
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-08-30 10:52:58 +0600

    database connection buildup

---
+++ config/database.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+let connected = false;
+const connectDB = async () => {
+  mongoose.set("strictQuery", true);
+  if (connected) {
+    console.log("Mongodb is connected");
+    return;
+  }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    connected = true;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default connectDB;
